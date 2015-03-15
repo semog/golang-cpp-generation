920d42726079dd3c7bcd24a710a0dda2d398cbfb
aag <code.aa@gdbl.me>
2015-03-15 19:49:49 +0530

    More renaming and comments. Added the gopp script

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
+// Run the pre-process script when go generate is run
 //go:generate gopp
 
-
-
+// The driver function
 func main() {
-	generic()
-	generic_sum()
+	reflected()
+	reflected_sum()
 	generated()
 	generated_sum()
 }
--- reflected.go
+++ reflected.go
@@ -7,7 +7,7 @@ import "fmt"
 // Also the list is not type safe. One can even do
 // `lst = lst.Cons("imastring")` and the compiler
 // wont complain and you will get a runtime panic
-func generic() {
+func reflected() {
 	lst := Cons(1)
 	lst = lst.Cons(Cons(2))
 	first := lst.Car().(int)
@@ -26,7 +26,7 @@ func (i Integer) Add(j Integer) Integer {
 	return Integer(i + j)
 }
 
-func generic_sum() {
+func reflected_sum() {
 	lst := Cons(Integer(1))
 	lst = lst.Cons(Cons(Integer(2)))
 	fmt.Println(lst.Sum().(Integer) + 1)
